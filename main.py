ac0a7d0a3231602b81e9eb46b7388100f461e070
zhangxuancheng <zhangxuancheng@corp.netease.com>
2025-07-16 10:58:14 +0800

    提交了对透明度的支持

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 import shutil
 from PIL import Image
-import imageio
 
 ATLAS_DIR = "Atlas"
 GIF_DIR = "GIF"
 SPLIT_DIR = "Split"
+
+BACKGROUND_COLOR = (0, 0, 0, 0)
 
 # clear directory
 def clear_directory(directory):
@@ -31,16 +32,20 @@ def fill_empty_image(image):
     for x in range(image.width):
         for y in range(image.height):
             r, g, b, a = image.getpixel((x, y))
-            if a == 0:
-                image.putpixel((x, y), (0, 0, 0, 255))
+            if a == 0 and BACKGROUND_COLOR[3] != 0:
+                image.putpixel((x, y), BACKGROUND_COLOR)
     return image
     
 
 def split(atlas_path, file_prefix, scale=1):
     atlas = Image.open(atlas_path)
+    atlas = atlas.transpose(Image.FLIP_TOP_BOTTOM)
     width, height = atlas.size
-    tile_width = 128
-    tile_height = 128
+    parent_dir = os.path.dirname(atlas_path)
+    parent_dir = os.path.basename(parent_dir)  # 获取Atlas目录下的子目录名
+    tile_size = parent_dir.split('x')
+    tile_width = int(tile_size[0]) if len(tile_size) > 0 else 128
+    tile_height = int(tile_size[1]) if len(tile_size) > 1 else 128
     rows = height // tile_height
     cols = width // tile_width
 
@@ -52,18 +57,18 @@ def split(atlas_path, file_prefix, scale=1):
             lower = upper + tile_height
             
             tile = atlas.crop((left, upper, right, lower))
-            tile_name = f"{file_prefix}_{col + row * cols}.png"
+            index_str = f"{col + row * cols:02d}"
+            tile_name = f"{file_prefix}_{index_str}.png"
             tile_path = os.path.join(SPLIT_DIR, tile_name)
-            if not is_empty_image(tile):
-                tile = fill_empty_image(tile)
+            if is_empty_image(tile):
+                continue
+            tile = fill_empty_image(tile)
             if scale != 1:
                 # 用nearest neighbor缩放
                 tile = tile.resize((tile_width * scale, tile_height * scale), Image.NEAREST)
-                # 颠倒
-                tile = tile.transpose(Image.FLIP_TOP_BOTTOM)
-                tile.save(tile_path)
+            tile.save(tile_path)
 
-# 使用imageio将分割的图片转换为GIF
+# 使用PIL库的imageio保存为GIF
 def create_gif_from_split(file_prefix):
     split_images = []
     for f in os.listdir(SPLIT_DIR):
@@ -74,14 +79,21 @@ def create_gif_from_split(file_prefix):
         return
     
     split_images.sort()  # 确保顺序正确
-    images = [imageio.imread(img) for img in split_images]
+    images = [Image.open(img) for img in split_images]
     gif_path = os.path.join(GIF_DIR, f"{file_prefix}.gif")
-    imageio.mimsave(gif_path, images, duration=0.3, loop=0)
+    images[0].save(gif_path, save_all=True, append_images=images[1:], duration=100, transparency=0, loop=0, disposal=2)
 
-pngs = [f for f in os.listdir(ATLAS_DIR) if f.endswith('.png')]
+pngs = []
+# 遍历Atlas目录下的所有png文件
+for root, dirs, files in os.walk(ATLAS_DIR):
+    for file in files:
+        if file.endswith('.png'):
+            full_path = os.path.join(root, file)
+            pngs.append(os.path.relpath(full_path, ATLAS_DIR))
 for png in pngs:
     atlas_path = os.path.join(ATLAS_DIR, png)
-    gif_name = os.path.splitext(png)[0]
+    gif_name = os.path.splitext(png)[-2]
+    gif_name = gif_name.split(os.sep)[-1]  # 获取文件名
     
     split(atlas_path, gif_name, 4)
     create_gif_from_split(gif_name)
